d9129c541cbf5f53e37b24c0da4316ec2e64489e
svr2kos2 <svr2kos2@hotmail.com>
2023-07-27 20:27:48 +0800

    Add osc comm with VRChat

--- DGLab_BLE/DGLab_BLE.cs
+++ DGLab_BLE/DGLab_BLE.cs
@@ -13,97 +13,103 @@ using System.Net;
 using System.Net.Sockets;
 using System.Reflection.Emit;
 
+namespace DGLab {
+    public class DGLab_BLE {
+        public DeviceInformation device;
+        public GattDeviceService battrySV;
+        public GattDeviceService controlSV;
+        public GattCharacteristic battry;
+        public GattCharacteristic level; //04 A 0-5 | B 6-11
+        public GattCharacteristic waveA; //06 
+        public GattCharacteristic waveB; //05 
 
-public class DGLab_BLE {
-    public DeviceInformation device;
-    public GattDeviceService battrySV;
-    public GattDeviceService controlSV;
-    public GattCharacteristic battry;
-    public GattCharacteristic level; //04 A 0-5 | B 6-11
-    public GattCharacteristic waveA; //06 
-    public GattCharacteristic waveB; //05 
+        public DGLab_BLE() {
+            try {
+                var battrySVSelector = GattDeviceService.GetDeviceSelectorFromUuid(new Guid("955A180A-0FE2-F5AA-A094-84B8D4F3E8AD"));
+                var controlSVSelector = GattDeviceService.GetDeviceSelectorFromUuid(new Guid("955A180B-0FE2-F5AA-A094-84B8D4F3E8AD"));
+                battrySV = AsyncResult(GattDeviceService.FromIdAsync(AsyncResult(DeviceInformation
+                    .FindAllAsync(battrySVSelector)).First().Id));
+                controlSV = AsyncResult(GattDeviceService.FromIdAsync(AsyncResult(DeviceInformation
+                    .FindAllAsync(controlSVSelector)).First().Id));
 
-    public DGLab_BLE() {
-        try {
-            var battrySVSelector = GattDeviceService.GetDeviceSelectorFromUuid(new Guid("955A180A-0FE2-F5AA-A094-84B8D4F3E8AD"));
-            var controlSVSelector = GattDeviceService.GetDeviceSelectorFromUuid(new Guid("955A180B-0FE2-F5AA-A094-84B8D4F3E8AD"));
-            battrySV = AsyncResult(GattDeviceService.FromIdAsync(AsyncResult(DeviceInformation
-                .FindAllAsync(battrySVSelector)).First().Id));
-            controlSV = AsyncResult(GattDeviceService.FromIdAsync(AsyncResult(DeviceInformation
-                .FindAllAsync(controlSVSelector)).First().Id));
-
-            battry = AsyncResult(battrySV.GetCharacteristicsForUuidAsync(new Guid("955A1500-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
-            level = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1504-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
-            waveA = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1506-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
-            waveB = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1505-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
-        } catch {
-            Console.WriteLine("BLE Connection faild.");
+                battry = AsyncResult(battrySV.GetCharacteristicsForUuidAsync(new Guid("955A1500-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
+                level = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1504-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
+                waveA = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1506-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
+                waveB = AsyncResult(controlSV.GetCharacteristicsForUuidAsync(new Guid("955A1505-0FE2-F5AA-A094-84B8D4F3E8AD"))).Characteristics.FirstOrDefault();
+            } catch {
+                Console.WriteLine("BLE Connection faild.");
+            }
         }
-    }
 
 
-    ~DGLab_BLE() {
-        battrySV.Dispose();
-        controlSV.Dispose();
-    }
-
-    public enum Channel { 
-        A,
-        B
-    }
-
-    public byte[] ReadBytes(GattCharacteristic ch) {
-        var buffer = AsyncResult(ch.ReadValueAsync()).Value;
-        byte[] current = new byte[buffer.Length];
-        DataReader.FromBuffer(buffer).ReadBytes(current);
-        return current;
-    }
-
-    public void WriteBytes(GattCharacteristic ch, byte[] value) {
-        IBuffer buffer = Windows.Security.Cryptography.CryptographicBuffer.CreateFromByteArray(value);
-        _ = ch.WriteValueAsync(buffer);
-    }
-
-    public (int,int) GetLevel() {
-        var bytes = ReadBytes(level);
-        var levelA = bytes[2] & 0x3F;
-        var levelB = (bytes[2] >> 6) | ((bytes[1] & 0x0F) << 2);
-        return (levelA, levelB);
-    }
-
-    public void SetLevel(Channel ch,byte value) {
-        var bytes = ReadBytes(level);
-        if(ch == Channel.A) {
-            bytes[2] = (byte)((bytes[2] & 0xC0) | (value & 0x3F));
-        } else {
-            bytes[2] = (byte)((bytes[2] & 0x3F) | (value << 6));
-            bytes[1] = (byte)(value >> 2);
+        ~DGLab_BLE() {
+            battrySV.Dispose();
+            controlSV.Dispose();
         }
-        WriteBytes(level, bytes);
-    }
 
-    public void SetWave(Channel ch, byte[] value) {
-        IBuffer buffer = Windows.Security.Cryptography.CryptographicBuffer.CreateFromByteArray(value);
-        if (ch == Channel.A) {
-            _ = waveA.WriteValueAsync(buffer);
-        } else {
-            _ = waveB.WriteValueAsync(buffer);
+        public enum Channel { 
+            A,
+            B
         }
-    }
+
+        public byte[] ReadBytes(GattCharacteristic ch) {
+            var buffer = AsyncResult(ch.ReadValueAsync()).Value;
+            byte[] current = new byte[buffer.Length];
+            DataReader.FromBuffer(buffer).ReadBytes(current);
+            return current;
+        }
+
+        public void WriteBytes(GattCharacteristic ch, byte[] value) {
+            IBuffer buffer = Windows.Security.Cryptography.CryptographicBuffer.CreateFromByteArray(value);
+            _ = ch.WriteValueAsync(buffer);
+        }
+
+        public (int,int) GetLevel() {
+            var bytes = ReadBytes(level);
+            var levelA = bytes[2] & 0x3F;
+            var levelB = (bytes[2] >> 6) | ((bytes[1] & 0x0F) << 2);
+            return (levelA, levelB);
+        }
+
+        public void SetLevel(Channel ch, int val) {
+            var power = ReadBytes(level);
+            if(ch == Channel.A) {
+                power[1] = (byte)((val << 3) | (power[1] & 7));
+                power[2] = (byte)(val >> 5);
+            } else {
+                power[0] = (byte)(val & 0xFF);
+                power[1] = (byte)((val >> 8) & 0xFF);
+            }
+            //var val = ((a & 0x7FF) << 11) | (b & 0x7FF);
+            //power[0] = (byte)(val & 0xFF);
+            //power[1] = (byte)((val >> 8) & 0xFF);
+            //power[2] = (byte)((val >> 16) & 0xFF);
+            WriteBytes(level, power);
+        }
+
+        public void SetWave(Channel ch, byte[] value) {
+            IBuffer buffer = Windows.Security.Cryptography.CryptographicBuffer.CreateFromByteArray(value);
+            if (ch == Channel.A) {
+                _ = waveA.WriteValueAsync(buffer);
+            } else {
+                _ = waveB.WriteValueAsync(buffer);
+            }
+        }
 
 
-    private static T AsyncResult<T>(IAsyncOperation<T> async) {
-        while (true) {
-            switch (async.Status) {
-                case AsyncStatus.Started:
-                    Thread.Sleep(10);
-                    continue;
-                case AsyncStatus.Completed:
-                    return async.GetResults();
-                case AsyncStatus.Error:
-                    throw async.ErrorCode;
-                case AsyncStatus.Canceled:
-                    throw new TaskCanceledException();
+        private static T AsyncResult<T>(IAsyncOperation<T> async) {
+            while (true) {
+                switch (async.Status) {
+                    case AsyncStatus.Started:
+                        Thread.Sleep(10);
+                        continue;
+                    case AsyncStatus.Completed:
+                        return async.GetResults();
+                    case AsyncStatus.Error:
+                        throw async.ErrorCode;
+                    case AsyncStatus.Canceled:
+                        throw new TaskCanceledException();
+                }
             }
         }
     }
--- DGLab_BLE/Program.cs
+++ DGLab_BLE/Program.cs
@@ -19,41 +19,74 @@ using System.Net.Sockets;
 using System.Diagnostics;
 using Microsoft.Win32;
 using System.Reflection;
+using DGLab;
 
-namespace BLE_HeartBeat
-{
-    class Program
-    {
+namespace BLE_HeartBeat {
+    class Program {
+        static float lBreast = 0;
+        static float rBreast = 0;
 
-        static void Main(string[] args)
-        {
+        static float touchSelf = 0;
+        static float touchOthers = 0;
+
+        static IPEndPoint localIpep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9001);
+        static IPEndPoint remotelpep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+        static UdpClient udp = new UdpClient(localIpep);
+
+        static void Main(string[] args) {
             DGLab_BLE dgLab = new DGLab_BLE();
-            var level = dgLab.GetLevel();
-            var b = dgLab.ReadBytes(dgLab.level);
-            Console.WriteLine(BitConverter.ToString(b));
-            Console.WriteLine(level.Item1.ToString("X2"));
-            Console.WriteLine(level.Item2.ToString("X2"));
+            udp.BeginReceive(RecvData, null);
 
-
-            dgLab.SetLevel(DGLab_BLE.Channel.A, 0); //max 63
-            dgLab.SetLevel(DGLab_BLE.Channel.B, 0); //max 63
+            dgLab.SetLevel(DGLab_BLE.Channel.A, 0);
+            dgLab.SetLevel(DGLab_BLE.Channel.B, 0);
+            
             for (; ; ) {
-                var cmd = Console.ReadLine();
-                if(int.TryParse(cmd,out var res)) {
-                    dgLab.SetLevel(DGLab_BLE.Channel.A, (byte)res);
-                } else {
-                    var bt = dgLab.ReadBytes(dgLab.level);
-                    Console.WriteLine(BitConverter.ToString(bt));
-                    dgLab.SetWave(DGLab_BLE.Channel.A, new byte[] { 0x0A, 0x08, 0x05 });
+                Console.WriteLine(DateTime.Now + " " + touchSelf + " " + lBreast + " " + rBreast);
+                var breast = Math.Max(lBreast, rBreast);
+                if (breast > 0.01f) {
+                    var t = breast / 0.1666f;
+                    dgLab.SetLevel(DGLab_BLE.Channel.B, (int)(100 + t * 400));
+                    dgLab.SetWave(DGLab_BLE.Channel.B, new byte[] { 0x61, 0x81, 0x02 });
                 }
+
+                var touch = Math.Max(touchSelf, touchOthers);
+                if (touch > 0.1f) {
+                    dgLab.SetLevel(DGLab_BLE.Channel.A, (int)(100 + touch * 400));
+                    dgLab.SetWave(DGLab_BLE.Channel.A, new byte[] { 0x61, 0x81, 0x02 });
+                }
+                
+                Thread.Sleep(100);
             }
-
-
-            dgLab.SetLevel(DGLab_BLE.Channel.A,8); //max 63
-            dgLab.SetLevel(DGLab_BLE.Channel.B,7); //max 63
-            dgLab.SetWave(DGLab_BLE.Channel.B, new byte[] { 0x0A, 0x08, 0x05 });
         }
 
-
+        static void RecvData(IAsyncResult ar) {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            var receivedData = udp.EndReceive(ar, ref remoteEndPoint).ToList();
+            List<byte> addressBytes = new List<byte>();
+            int i = 0;
+            for (; i < receivedData.Count; ++i) {
+                if (receivedData[i] == ',')
+                    break;
+                addressBytes.Add(receivedData[i]);
+            }
+            var address = Encoding.UTF8.GetString(addressBytes.ToArray());
+            i += 2;
+            var type = receivedData[i];
+            i += 2;
+            var valueBytes = receivedData.GetRange(i, receivedData.Count - i);
+            valueBytes.Reverse();
+            float value = 0;
+            if (valueBytes != null && valueBytes.Count >= 4)
+                value = BitConverter.ToSingle(valueBytes.ToArray(), 0);
+            if (address.Contains("LBreast_Angle"))
+                lBreast = value;
+            if (address.Contains("RBreast_Angle"))
+                rBreast = value;
+            if (address.Contains("Oriface_Hole/TouchSelf"))
+                touchSelf = value;
+            if (address.Contains("Oriface_Hole/TouchOthers"))
+                touchOthers = value;
+            udp.BeginReceive(RecvData, null);
+        }
     }
 }
